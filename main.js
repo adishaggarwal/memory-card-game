7bc9696024a02e131cf62002d1d667a74a51bf07
adish aggarwal <aggarwaladish@gmail.com>
2019-09-18 12:05:12 +0530

    game

---
+++ main.js
@@ -0,0 +1,70 @@
+const cards=document.querySelectorAll('.box');
+
+(function shuffle()
+{
+    console.log('hello');
+    cards.forEach(card =>{
+        let randnum=Math.floor(Math.random()*12);
+        card.style.order=randnum;
+    })
+})();
+
+cards.forEach(card => card.addEventListener('click',flipcard));
+
+
+
+let hasflipped=false;
+let firstcard,secondcard;
+var lock=false;
+
+
+function flipcard()
+{
+    if(lock)
+    {
+        return;
+    }
+    
+    var x=this;
+    this.classList.toggle('hide1');
+    setTimeout(function(){
+        x.classList.toggle('hide1');
+    }, 1500);
+
+    if(!hasflipped)
+    {
+        hasflipped=true;
+        firstcard=this;
+    }
+    else
+    {
+        if(this==firstcard)
+        {
+            
+        }
+        else
+        {
+            hasflipped=false;
+            secondcard=this;
+    
+            lock=true;
+        }
+       
+
+    if(firstcard.dataset.img==secondcard.dataset.img)
+    {
+        firstcard.classList.add('show1');
+        secondcard.classList.add('show1');
+    }
+    
+
+    setTimeout(function() {
+        lock=false;
+    }, 1600);
+    }
+}
+
+   
+
+
+    
